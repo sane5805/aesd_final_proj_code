3d096d755e82adc18a3b7b0acb31881cb39a6eab
saurav negi <sane5805@colorado.edu>
2024-04-23 19:57:15 +0000

    socket and test sensor changes

--- base_external/package/socket-communication/socket-communication.mk
+++ base_external/package/socket-communication/socket-communication.mk
@@ -1,26 +1,26 @@
 
 ##############################################################
 #
-# TEMP-SENSOR
+# SOCKET-COMMUNICATION
 #
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-TEMP_SENSOR_VERSION = 47094d514789a40e014d78293a04ea0b8962524f
+SOCKET_COMMUNICATION_VERSION = 47094d514789a40e014d78293a04ea0b8962524f 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-TEMP_SENSOR_SITE = git@github.com:sane5805/aesd_final_proj_utils.git
-TEMP_SENSOR_SITE_METHOD = git
-TEMP_SENSOR_GIT_SUBMODULES = YES
+SOCKET_COMMUNICATION_SITE = git@github.com:sane5805/aesd_final_proj_utils.git
+SOCKET_COMMUNICATION_SITE_METHOD = git
+SOCKET_COMMUNICATION_GIT_SUBMODULES = YES
 
-define TEMP_SENSOR_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/temp-sensor all
+define SOCKET_COMMUNICATION_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server-socket all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
-define TEMP_SENSOR_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/temp-sensor/temp_sensor $(TARGET_DIR)/usr/bin
+define SOCKET_COMMUNICATION_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/server-socket/server $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
